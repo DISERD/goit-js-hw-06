d231508c062265d01814236ad6cdff497333c2c9
Daniel <puricchdaniil@gmail.com>
2026-06-02 00:43:33 +0300

    Update HTML structure and add JavaScript functionality for tasks

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Завдання 1
+const input = document.getElementById("inputText");
+const button = document.getElementById("btn");
+
+button.textContent = input.value;
+
+// Завдання 2
+const image = document.getElementById("image");
+
+image.src = "new.jpg";
+
+// Завдання 3
+const link = document.getElementById("link");
+const photo = document.getElementById("photo");
+
+link.href = "https://uk.wikipedia.org";
+photo.alt = "Приклад зображення";
+
+// Завдання 4
+const firstItem = document.getElementById("first-item");
+
+firstItem.textContent = "Новий текст";
